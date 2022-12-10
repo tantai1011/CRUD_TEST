88a91cf2ad612d1ed6e5db459b53e046123d80c8
Tran Tan Tai <trantantai10111997@live.com>
2022-12-10 15:07:18 +0700

    add ajax update panel

--- CRUD_InASP/Default.aspx.designer.cs
+++ CRUD_InASP/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace CRUD_InASP
     {
 
         /// <summary>
+        /// UpdatePanel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
